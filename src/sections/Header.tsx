7a5b39946ac4d8785732b84572d9431cbf81eab1
Lucas Oliveira <lucazolvr@gmail.com>
2025-02-26 11:31:47 -0300

    ajuestes navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,20 @@
 export const Header = () => {
-  return <div>
-    <nav>
-      <a href="#">Início</a>
-      <a href="#">Projetos</a>
-      <a href="#">Sobre</a>
-      <a href="#">Contato</a>
-    </nav>
-  </div>;
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full">
+        <a href="#" className="nav-item">
+          Início
+        </a>
+        <a href="#" className="nav-item">
+          Projetos
+        </a>
+        <a href="#" className="nav-item">
+          Sobre
+        </a>
+        <a href="#" className="nav-item">
+          Contato
+        </a>
+      </nav>
+    </div>
+  );
 };
