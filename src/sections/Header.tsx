69198802c366c9367fa31d0d3fb554c37fbace1e
Lucas Oliveira <lucazolvr@gmail.com>
2025-02-26 11:15:25 -0300

    commit inicial

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return <div>
+    <nav>
+      <a href="#">Início</a>
+      <a href="#">Projetos</a>
+      <a href="#">Sobre</a>
+      <a href="#">Contato</a>
+    </nav>
+  </div>;
+};
