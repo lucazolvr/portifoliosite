fe5bfc0d8c4f20be1eefb75b1e3329e6ebc624df
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-01 19:24:21 -0300

    criação da hero ring e ajuste no esquema de cores

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,18 +1,30 @@
 import memojiImage from '@/assets/images/memoji- notebook.png';
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+import grainImage from '@/assets/images/grain.jpg';
 
 export const HeroSection = () => {
   return (
-  <div className="py-32 md:py-48">
+  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="absolute inset-0 -z-30 opacity-5"
+     style={{
+      backgroundImage: `url(${grainImage.src})`,
+    }}></div>
+
+    <div className="size-[620px] hero-ring"></div>
+    <div className="size-[820px] hero-ring"></div>
+    <div className="size-[1020px] hero-ring"></div>
+    <div className="size-[1220px] hero-ring"></div>
+
     <div className="container">
       <div className="flex flex-col items-center">
       <Image src={memojiImage} className="size-[100px]" alt='lucas usando notebook' />
-      <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+      <div className="bg-blue-950 border border-blue-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
         <div className="bg-green-500 size-2.5 rounded-full"></div>
         <div className="text-sm font-medium">Disponível para trabalhos</div>
       </div>
       </div>
+
       <div className="max-w-lg mx-auto">
       <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
         Transformando Idéias em Realidade
@@ -22,12 +34,13 @@ export const HeroSection = () => {
         aplicaçõess de qualidade. Vamos conversar sobre seu novo projeto.
       </p>
       </div>
+
       <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
         <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
           <span className="font-semibold">Explore Meu Portifólio</span>
           <ArrowDown className="size-4"/>
         </button>
-        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-blue-900 h-12 px-6 rounded-xl">
           <span>👋</span>
           <span className="font-semibold">Vamos Conversar</span>
         </button>
