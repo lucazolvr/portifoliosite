75164e144cadb9f7d5335eb35bb167120b1f6335
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-01 12:06:13 -0300

    mudanças na header section e hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-  <div className="py-32">
+  <div className="py-32 md:py-48">
     <div className="container">
       <div className="flex flex-col items-center">
       <Image src={memojiImage} className="size-[100px]" alt='lucas usando notebook' />
@@ -13,14 +13,16 @@ export const HeroSection = () => {
         <div className="text-sm font-medium">Disponível para trabalhos</div>
       </div>
       </div>
-      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+      <div className="max-w-lg mx-auto">
+      <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
         Transformando Idéias em Realidade
         </h1>
-      <p className="mt-4 text-center text-white/60">
+      <p className="mt-4 text-center text-white/60 md:text-lg">
         Estou aqui pra tirar seus sonhos do papel e transformar em
         aplicaçõess de qualidade. Vamos conversar sobre seu novo projeto.
       </p>
-      <div className="flex flex-col items-center mt-8 gap-4">
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
         <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
           <span className="font-semibold">Explore Meu Portifólio</span>
           <ArrowDown className="size-4"/>
