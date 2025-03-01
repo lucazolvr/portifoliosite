4fe0ed9f29da6837f002a538ff3bb169e6c9e577
Lucas Oliveira <lucazolvr@gmail.com>
2025-02-28 21:14:16 -0300

    mudanças na header section e hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,36 @@
+import memojiImage from '@/assets/images/memoji- notebook.png';
+import Image from 'next/image';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+  <div className="py-32">
+    <div className="container">
+      <div className="flex flex-col items-center">
+      <Image src={memojiImage} className="size-[100px]" alt='lucas usando notebook' />
+      <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+        <div className="bg-green-500 size-2.5 rounded-full"></div>
+        <div className="text-sm font-medium">Disponível para trabalhos</div>
+      </div>
+      </div>
+      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+        Transformando Idéias em Realidade
+        </h1>
+      <p className="mt-4 text-center text-white/60">
+        Estou aqui pra tirar seus sonhos do papel e transformar em
+        aplicaçõess de qualidade. Vamos conversar sobre seu novo projeto.
+      </p>
+      <div className="flex flex-col items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Explore Meu Portifólio</span>
+          <ArrowDown className="size-4"/>
+        </button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <span>👋</span>
+          <span className="font-semibold">Vamos Conversar</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  );
 };
