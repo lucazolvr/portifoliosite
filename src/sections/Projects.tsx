7fcea96a5258a0c34c3101e03d7b42bbf8afe59c
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-11 10:09:40 -0300

    enhance ProjectsSection layout with responsive adjustments; add utility class for container and improve spacing

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -47,7 +47,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center ">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-purple-400 via-sky-400 to-teal-400 text-center text-transparent bg-clip-text">
@@ -57,8 +57,10 @@ export const ProjectsSection = () => {
         <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Projetos em Destaque
         </h2>
-        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md
-          mx-auto">
+        <p
+          className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md
+          mx-auto"
+        >
           Veja como posso transformar simples pensamentos em grandes projetos.
         </p>
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
@@ -68,7 +70,7 @@ export const ProjectsSection = () => {
               className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
            after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2
             after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20
-             px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none"
+             px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5"
@@ -76,14 +78,17 @@ export const ProjectsSection = () => {
                   backgroundImage: `url(${grainImage.src})`,
                 }}
               ></div>
-
+                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                  <div className="lg:pb-16">
               <div className="bg-gradient-to-r from-purple-400 to-teal-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
                 <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
 
-              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                {project.title}
+              </h3>
               <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
               <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                 {project.results.map((result) => (
@@ -100,11 +105,16 @@ export const ProjectsSection = () => {
                   <ArrowUpRightIcon className="size-4" />
                 </button>
               </a>
+              </div>
+              <div className="relative">
               <Image
                 src={project.image}
                 alt={project.title}
-                className="mt-8 -mb-4"
+                className="mt-8 -mb-4 md:-mb-0 lg:mt-0
+                 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
               />
+              </div>
+              </div>
             </div>
           ))}
         </div>
