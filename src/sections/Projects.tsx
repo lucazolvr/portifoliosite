6b69ee6638ff945d3d274a54c27141b27f023f7f
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-15 17:39:26 -0300

    add Card component for reusable project card layout; refactor ProjectsSection to utilize Card component for improved structure

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,6 +6,7 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -57,19 +58,11 @@ export const ProjectsSection = () => {
         />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
-            <div
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
-           after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2
-            after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20
-             px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+              className="
+             px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
             >
-              <div
-                className="absolute inset-0 -z-10 opacity-5"
-                style={{
-                  backgroundImage: `url(${grainImage.src})`,
-                }}
-              ></div>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-purple-400 to-teal-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
@@ -107,7 +100,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
