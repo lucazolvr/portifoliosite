02aaf5013985efed1120abdd4d29bd6b2f4684d6
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-15 12:25:55 -0300

    refactor HeroSection and ProjectsSection for improved structure; add SectionHeader component for consistent section titles

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,27 @@
-import memojiImage from '@/assets/images/memoji- notebook.png';
-import Image from 'next/image';
-import ArrowDown from '@/assets/icons/arrow-down.svg';
-import grainImage from '@/assets/images/grain.jpg';
-import SpaceshipIncon from '@/assets/icons/space-ship-space-svgrepo-com.svg';
-import UfoImage from '@/assets/icons/noun-ufo-6642260.svg';
-import { HeroOrbit } from '@/components/HeroOrbit';
+import memojiImage from "@/assets/images/memoji- notebook.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+// import SpaceshipIncon from '@/assets/icons/space-ship-space-svgrepo-com.svg';
+// import UfoImage from '@/assets/icons/noun-ufo-6642260.svg';
+import { HeroOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
   return (
-  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-     <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
-    <div className="absolute inset-0 -z-30 opacity-5"
-     style={{
-      backgroundImage: `url(${grainImage.src})`,
-    }}></div>
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
 
-    <div className="size-[620px] hero-ring"></div>
-    <div className="size-[820px] hero-ring"></div>
-    <div className="size-[1020px] hero-ring"></div>
-    <div className="size-[1220px] hero-ring"></div>
-    <HeroOrbit size={720} rotation={-72}>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        {/* <HeroOrbit size={720} rotation={-72}>
       <SpaceshipIncon className="size-16 fill-current text-teal-300"/>
       </HeroOrbit>
     <HeroOrbit size={550} rotation={-14}>
@@ -48,42 +50,44 @@ export const HeroSection = () => {
       </HeroOrbit>
     <HeroOrbit size={650} rotation={-5}>
       <div className="size-3 rounded-full bg-emerald-300/20"/>
-      </HeroOrbit>
+      </HeroOrbit> */}
       </div>
 
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="lucas usando notebook"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="absolute bg-green-400 size-2.5 animate-ping rounded-full opacity-75"></div>
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">Disponível para trabalhos</div>
+          </div>
+        </div>
 
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Transformando Idéias em Realidade
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            Estou aqui pra tirar seus sonhos do papel e transformar em
+            aplicaçõess de qualidade. Vamos conversar sobre seu novo projeto.
+          </p>
+        </div>
 
-    <div className="container">
-      <div className="flex flex-col items-center">
-      <Image src={memojiImage} className="size-[100px]" alt='lucas usando notebook' />
-      <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-        <div className="absolute bg-green-400 size-2.5 animate-ping rounded-full opacity-75"></div>
-        <div className="bg-green-500 size-2.5 rounded-full"></div>
-        <div className="text-sm font-medium">Disponível para trabalhos</div>
-      </div>
-      </div>
-
-      <div className="max-w-lg mx-auto">
-      <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-        Transformando Idéias em Realidade
-        </h1>
-      <p className="mt-4 text-center text-white/60 md:text-lg">
-        Estou aqui pra tirar seus sonhos do papel e transformar em
-        aplicaçõess de qualidade. Vamos conversar sobre seu novo projeto.
-      </p>
-      </div>
-
-      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-          <span className="font-semibold">Explore Meu Portifólio</span>
-          <ArrowDown className="size-4"/>
-        </button>
-        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-          <span>👋</span>
-          <span className="font-semibold">Vamos Conversar</span>
-        </button>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore Meu Portifólio</span>
+            <ArrowDown className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+            <span>👋</span>
+            <span className="font-semibold">Vamos Conversar</span>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
+import { SectionHeader } from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
@@ -49,20 +50,11 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center ">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-purple-400 via-sky-400 to-teal-400 text-center text-transparent bg-clip-text">
-            Resultados Reais
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-          Projetos em Destaque
-        </h2>
-        <p
-          className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md
-          mx-auto"
-        >
-          Veja como posso transformar simples pensamentos em grandes projetos.
-        </p>
+        <SectionHeader
+          eyebrow="Resultados Reais"
+          title="Projetos em Destaque"
+          description="Veja como posso transformar simples pensamentos em grandes projetos."
+        />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
             <div
@@ -78,42 +70,42 @@ export const ProjectsSection = () => {
                   backgroundImage: `url(${grainImage.src})`,
                 }}
               ></div>
-                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                  <div className="lg:pb-16">
-              <div className="bg-gradient-to-r from-purple-400 to-teal-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
-              </div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-purple-400 to-teal-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
 
-              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
-                {project.title}
-              </h3>
-              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                {project.results.map((result) => (
-                  // eslint-disable-next-line react/jsx-key
-                  <li className="flex gap-2 text-sm md:text-base text-white/50">
-                    <CheckCircleIcon className="size-5 md:size-6" />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a href="{project.link}">
-                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                  <span>Abrir Projeto</span>
-                  <ArrowUpRightIcon className="size-4" />
-                </button>
-              </a>
-              </div>
-              <div className="relative">
-              <Image
-                src={project.image}
-                alt={project.title}
-                className="mt-8 -mb-4 md:-mb-0 lg:mt-0
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      // eslint-disable-next-line react/jsx-key
+                      <li className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href="{project.link}">
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Abrir Projeto</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0
                  lg:absolute lg:h-full lg:w-auto lg:max-w-none"
-              />
-              </div>
+                  />
+                </div>
               </div>
             </div>
           ))}
