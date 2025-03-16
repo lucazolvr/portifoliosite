3d7a587dc0d588e726ad25014dd55e72b023b113
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-16 01:06:52 -0300

    remove unused book cover and chrome icon; add spring icon and enhance AboutSection with toolbox items and new Card components for improved content presentation

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,48 @@
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/LivroArqSoftw.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg"
+import HTMLIcon from "@/assets/icons/html5.svg"
+import CSSIcon from "@/assets/icons/css3.svg"
+import ReactIcon from "@/assets/icons/react.svg"
+import JavaIcon from "@/assets/icons/icons8-java.svg"
+import GithubIcon from "@/assets/icons/github.svg"
+import SpringIcon from "@/assets/icons/spring.svg"
+
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    icon: "",
+  },
+  {
+    title: "HTML5",
+    icon: "",
+  },
+  {
+    title: "CSS3",
+    icon: "",
+  },
+  {
+    title: "React",
+    icon: "",
+  },
+  {
+    title: "Java",
+    icon: "",
+  },
+  {
+    title: "Github",
+    icon: "",
+  },
+  {
+    title: "SpringBoot",
+    icon: "",
+  },
+]
+
 
 export const AboutSection = () => {
   return (
@@ -9,6 +52,29 @@ export const AboutSection = () => {
         title="Uma Visão Do Meu Universo"
         description="Saiba mais sobre mim, o que eu faço e o que me inspira"
       />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Minhas Leituras</h3>
+            <p>Explore os livros moldam meu conhecimento.</p>
+          </div>
+          <Image src={bookImage} alt="capa livro" />
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Minhas Ferramentas</h3>
+            <p>
+              Explore as tecnologias e ferramentas que uso para desenvolver
+              projetos incríveis.
+            </p>
+          </div>
+          <div>
+
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
