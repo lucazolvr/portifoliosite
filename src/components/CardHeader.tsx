1c273ff11759101d1dc854c372c79ebce297f6db
Lucas Oliveira <lucazolvr@gmail.com>
2025-03-22 00:25:37 -0300

    refactor AboutSection and CardHeader for improved layout and styling; enhance hobbies section with absolute positioning and gradient backgrounds

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -12,7 +12,7 @@ export const CardHeader = ({
   return (
     <div className={twMerge("flex flex-col", className)}>
       <div className="inline-flex items-center gap-2">
-        <StarIcon className="size-9 text-emerald-300" />
+        <StarIcon className="size-9 text-purple-300" />
         <h3 className="font-serif text-3xl">{title}</h3>
       </div>
       <p className="text-sm text-white/60 mt-2">{description}</p>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -56,30 +56,44 @@ const hobbies = [
   {
     title: "Jogos",
     emoji: "🕹️",
+    left: "5%",
+    top: "5%",
   },
   {
     title: "Leitura",
     emoji: "📚",
-  },
-  {
-    title: "Música",
-    emoji: "🎵",
+    left: "50%",
+    top: "5%",
   },
   {
     title: "One Piece",
     emoji: "👒",
+    left: "35%",
+    top: "40%",
   },
   {
-    title: "Formula 1",
+    title: "Música",
+    emoji: "🎵",
+    left: "10%",
+    top: "35%",
+  },
+  {
+    title: "Formula1",
     emoji: "🏎️",
+    left: "70%",
+    top: "45%",
   },
   {
     title: "Basquete",
     emoji: "⛹️",
+    left: "5%",
+    top: "65%",
   },
   {
     title: "Academia",
     emoji: "🏋️‍♂️",
+    left: "45%",
+    top: "70%",
   },
 ];
 
@@ -92,7 +106,7 @@ export const AboutSection = () => {
           title="Uma Visão Do Meu Universo"
           description="Saiba mais sobre mim, o que eu faço e o que me inspira"
         />
-        <div className="mt-20 flex flex-col gap-6">
+        <div className="mt-20 flex flex-col gap-8">
           <Card className="h-[320px]">
             <CardHeader
               title="Minhas Leituras"
@@ -115,15 +129,25 @@ export const AboutSection = () => {
               itemsWrapperClassName="-translate-x-1/2"
             />
           </Card>
-          <Card>
+          <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
               title="Além do Código"
               description="Explore meus interesses e hobbies além do universo de códigos."
+              className="px-6 py-6"
             />
-            <div>
+            <div className="relative flex-1">
               {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
+                <div
+                  key={hobby.title}
+                  className="inline-flex gap-2 items-center px-6 bg-gradient-to-r from-purple-400 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobby.title}
+                  </span>
                   <span>{hobby.emoji}</span>
                 </div>
               ))}
